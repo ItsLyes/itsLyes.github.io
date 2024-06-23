2947a8dc92986c8ea37be0f27fde216c2fc58920
ItsLyes <lyes.hlrs@gmail.com>
2024-06-23 18:20:17 -0400

    color background

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 window.onload = async () => {
 // Appel de la fonction triangle avec des paramètres spécifiques
-triangle("draw", 100, 10, "#47a898");
+triangle("draw", 100, 10, "#e43f5a");
 }
 
